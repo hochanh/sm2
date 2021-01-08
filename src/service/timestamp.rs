32f2fe3b1bd1fe3594c21b8ff4b64521744451e6
manhtai <vo@manhtai.com>
2021-01-08 11:14:52 +0700

    Test for timestamp

--- src/service/timestamp.rs
+++ src/service/timestamp.rs
@@ -51,3 +51,63 @@ fn fixed_offset_from_minutes(minutes_west: i32) -> FixedOffset {
 fn local_minutes_west_for_stamp(stamp: i64) -> i32 {
     Local.timestamp(stamp, 0).offset().utc_minus_local() / 60
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::{Local, TimeZone, Utc};
+
+    // static timezone for tests
+    const AEST_MINS_WEST: i32 = -600;
+
+    #[test]
+    fn fixed_offset() {
+        let offset = fixed_offset_from_minutes(AEST_MINS_WEST);
+        assert_eq!(offset.utc_minus_local(), AEST_MINS_WEST * 60);
+    }
+
+    #[test]
+    #[cfg(target_vendor = "apple")]
+    /// On Linux, TZ needs to be set prior to the process being started to take effect,
+    /// so we limit this test to Macs.
+    fn local_minutes_west() {
+        // -480 throughout the year
+        std::env::set_var("TZ", "Australia/Perth");
+        assert_eq!(local_minutes_west_for_stamp(Utc::now().timestamp()), -480);
+    }
+
+    #[test]
+    fn next_day_at() {
+        let rollhour = 4;
+
+        // before the rollover, the next day should be later on the same day
+        let now = Local.ymd(2019, 1, 3).and_hms(2, 0, 0);
+        let next_day_at = Local.ymd(2019, 1, 3).and_hms(rollhour, 0, 0);
+        let today = get_next_day(
+            now.timestamp(),
+            now.offset().utc_minus_local() / 60,
+            rollhour as u8,
+        );
+        assert_eq!(today.timestamp(), next_day_at.timestamp());
+
+        // after the rollover, the next day should be the next day
+        let now = Local.ymd(2019, 1, 3).and_hms(rollhour, 0, 0);
+        let next_day_at = Local.ymd(2019, 1, 4).and_hms(rollhour, 0, 0);
+        let today = get_next_day(
+            now.timestamp(),
+            now.offset().utc_minus_local() / 60,
+            rollhour as u8,
+        );
+        assert_eq!(today.timestamp(), next_day_at.timestamp());
+
+        // after the rollover, the next day should be the next day
+        let now = Local.ymd(2019, 1, 3).and_hms(rollhour + 3, 0, 0);
+        let next_day_at = Local.ymd(2019, 1, 4).and_hms(rollhour, 0, 0);
+        let today = get_next_day(
+            now.timestamp(),
+            now.offset().utc_minus_local() / 60,
+            rollhour as u8,
+        );
+        assert_eq!(today.timestamp(), next_day_at.timestamp());
+    }
+}
